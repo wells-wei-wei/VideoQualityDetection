06883c1d1a2c1fd69e00257e0e3036e539fb5cc3
wells-wei-wei <conan19961005@163.com>
2020-09-11 13:44:54 +0800

    sqlite to mysql

--- VideoQualityDetection/settings.py
+++ VideoQualityDetection/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'VideoQualityDetection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'VideoQualityDetection',
+        'USER':'django',
+        'PASSWORD':'Wdsr19961005.',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
